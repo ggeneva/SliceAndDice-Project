311253278012406dba417cdb84fbb4854eab789e
ggeneva <g.geneva94@gmail.com>
2017-10-10 13:52:20 +0300

    refactor gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,81 @@
+/* global $ */
+
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
-const uglify = require('gulp-uglify');
-const pump = require('pump');
-// const concat = require('gulp-concat');
-// const clean = require('gulp-clean');
 
+gulp.task('configs', function() {
+    gulp.src('app/configs/*.js')
+        .pipe(babel({
+            presets: ['env'],
+        }))
+        .pipe($.minify({
+            ext: {
+                src: '.js',
+                min: '.js',
+            },
+            noSource: {},
+        }))
+        .pipe(gulp.dest('dist/configs'));
+});
+
+gulp.task('scripts', function() {
+    gulp.src('app/scripts/**/*.js')
+        .pipe(babel({
+            presets: ['env'],
+        }))
+        .pipe($.minify({
+            ext: {
+                src: '.js',
+                min: '.js',
+            },
+            noSource: {},
+        }))
+        .pipe(gulp.dest('dist/scripts'));
+});
 
 gulp.task('styles', function() {
+    gulp.src('app/styles/main.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('dist/styles'));
+});
+
+gulp.task('html', function() {
+    gulp.src('app/index.html')
+        .pipe($.htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function() {
+     gulp.src('app/images/*')
+        .pipe($.imagemin())
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('views', function() {
+    gulp.src('app/views/**/*')
+        .pipe(gulp.dest('dist/views'));
+});
+
+gulp.task('fonts', function() {
+    gulp.src('app/fonts/*')
+        .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('build:dist',
+['scripts', 'styles', 'views', 'html', 'configs', 'fonts', 'images']);
+
+
+// Sass
+gulp.task('styles-sass', function() {
     gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./app/styles/'));
-});
-
-gulp.task('compile:js', () => {
-    return gulp.src('./app/**/**/*.js')
-            .pipe(babel({
-                presets: ['env'],
-            }))
-            .pipe(gulp.dest('./build/js'));
-});
-
-gulp.task('minify:css', () => {
-    return gulp.src('./app/styles/**/*.css')
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(gulp.dest('./tmp/styles'));
-});
-
-gulp.task('minify:js', ['compile:js'], () => {
-    return pump([
-        gulp.src('./build/js/**/*.js'),
-        uglify(),
-        gulp.dest('./dist/scripts'),
-    ]);
+        .pipe(gulp.dest('./tmp/app/styles/'));
 });
 
 // Watch task
 gulp.task('default', function() {
-    gulp.watch('./sass/**/*.scss', ['styles']);
+    gulp.watch('./sass/**/*.scss', ['styles-sass']);
     gulp.watch('./app/**/**/*.js', ['compile:js']);
 });
